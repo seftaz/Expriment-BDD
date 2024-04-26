be2df6d9a7a4bda5fa7e225130683d5e12d42ffa
Arad Maleki <arad.m.t02@gmail.com>
2024-04-26 23:04:10 +0330

    added new step defs

--- BDD-Project/src/main/java/calculator/Calculator.java
+++ BDD-Project/src/main/java/calculator/Calculator.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public int calculate(int value1, int value2, char opt) {
+        if (opt == '*')
+            return value1 * value2;
+        if (opt == '/')
+            return value1 / value2;
+        if (opt == '^') {
+            int result = 1;
+            for (int i = 0; i < value2; i++)
+                result *= value1;
+            return result;
+        }
+        throw new NotImplementedException();
     }
 }
